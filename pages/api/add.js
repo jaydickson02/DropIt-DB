6ffefece7e85ec5cf49e19059fa276713d0ccca0
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2020-11-14 21:17:16 +1100

    Added Post Req Code

---
+++ pages/api/add.js
@@ -0,0 +1,35 @@
+import { connectToDatabase } from "../../util/mongodb";
+import Cors from 'cors'
+import initMiddleware from '../../lib/init_middleware'
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    origin: false,
+    // Only allow requests with GET, POST and OPTIONS
+    
+  })
+)
+
+export default async (req, res) => {
+
+  await cors(req, res);
+
+  const { db } = await connectToDatabase();
+
+  if (req.method === 'POST') {
+    console.log(req.body)
+  }
+
+  if(dbEntry){
+
+  await db.collection("Devices").insertOne(dbEntry, (err, res) => {
+        if (err) throw err;
+        console.log("1 document inserted")
+      });
+
+      res.send("1 document inserted");
+      
+    }
+};
