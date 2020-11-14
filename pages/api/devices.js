daa6a8fec77259918fa81ce8be96e4bfd81728e9
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2020-11-14 18:44:42 +1100

    Add CORS

--- pages/api/devices.js
+++ pages/api/devices.js
@@ -1,6 +1,20 @@
 import { connectToDatabase } from "../../util/mongodb";
+import Cors from 'cors'
+import initMiddleware from '../../lib/init-middleware'
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
 
 export default async (req, res) => {
+
+  await cors(req, res);
+
   const { db } = await connectToDatabase();
 
   const devices = await db
