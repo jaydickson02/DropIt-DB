003b33c39c67654340f91b894cfcc8cc70d444e0
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2020-11-14 18:58:57 +1100

    Update to allow all origins

--- pages/api/devices.js
+++ pages/api/devices.js
@@ -6,6 +6,7 @@ import initMiddleware from '../../lib/init_middleware'
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
+    origin: false,
     // Only allow requests with GET, POST and OPTIONS
     methods: ['GET', 'POST', 'OPTIONS'],
   })
