184db79460f964703ec42a9bd950f0b348a46733
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2020-11-15 17:58:00 +1100

    Skip passing students in Post request. Get it server side instead.

--- pages/api/update.js
+++ pages/api/update.js
@@ -18,27 +18,29 @@ export default async (req, res) => {
 
   if (req.method === 'POST') {
 
+    let students;
     let updatedValues = {};
 
     for(var key in req.body){
         if(req.body[key] && key != 'idSerialNumber' && key != 'prevStudents'){
+        if(req.body[key] && key != 'idSerialNumber'){
             updatedValues[key] = req.body[key]
         }
     }
 
+    const { db } = await connectToDatabase();
+
     //This is super bad, please fix later. Was temp for testing. See newField.js in DropIt App.
     for(var key in updatedValues){
         if(key == 'students'){
-            let studentArray = req.body.prevStudents.list;
-            console.log(studentArray);
-            console.log(studentArray.length);
-            studentArray.push({name: updatedValues[key], date: '10/10/2010', id:'123'})
-            updatedValues[key] = studentArray;
+            const device = await db.collection("Devices").find({serialNumber: idSerialNumber}).toArray();
+            students = device[0][students];
+            students.push({name: updatedValues[key], date: '10/10/2010', id:'123'})
         }
     }
-
-
-    const { db } = await connectToDatabase();
+    
+    console.log(students)
+    
 
     await db.collection("Devices").updateOne(
         { serialNumber: req.body.idSerialNumber },
